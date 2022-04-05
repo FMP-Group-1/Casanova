badb1893a7a0864e5f762a4f8d6f48380e21fa17
Charlie Taylor <charlie.taylor2@mail.bcu.ac.uk>
2022-04-05 16:03:40 +0100

    Renamed Melee Script to remove copies

--- Charlie's Stuff/FMP Template/Assets/Scripts/melee.cs
+++ Charlie's Stuff/FMP Template/Assets/Scripts/melee.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class meleeCC : MonoBehaviour
+public class melee : MonoBehaviour
 {
     [SerializeField]
     private GameObject holsteredSword;
