e7d5c0f47b9c731ebfab694566e80e397001a322
Charlie Taylor <charlietaylor257@gmail.com>
2022-05-09 17:32:28 +0100

    Prepping for build
    Made level manager a prefab, with the ability to reset scene and quit the game

--- Project Casanova/Assets/Charlie's Stuff/Scripts/LevelManager.cs
+++ Project Casanova/Assets/Charlie's Stuff/Scripts/LevelManager.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    //Input actions
+    [SerializeField]
+    private InputActionReference m_resetLevel;
+    [SerializeField]
+    private InputActionReference m_quitGame;
+
     /**************************************************************************************
     * Type: Function
     * 
@@ -17,7 +25,50 @@ public class LevelManager : MonoBehaviour
     **************************************************************************************/
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
+
+    /**************************************************************************************
+    * Type: Function
+    * 
+    * Name: OnEnable
+    * Parameters: n/a
+    * Return: n/a
+    *
+    * Author: Charlie Taylor
+    *
+    * Description: Enable input actions
+    **************************************************************************************/
+    private void OnEnable()
+	{
+        m_resetLevel.action.Enable();
+        m_quitGame.action.Enable();
+
+    }
+
+	/**************************************************************************************
+    * Type: Function
+    * 
+    * Name: Update
+    * Parameters: n/a
+    * Return: n/a
+    *
+    * Author: Charlie Taylor
+    *
+    * Description: Reset or Quit Game
+    **************************************************************************************/
+	private void Update()
+	{
+        //Reset Level
+		if ( m_resetLevel.action.triggered )
+		{
+            Scene scene = SceneManager.GetActiveScene(); 
+            SceneManager.LoadScene( scene.name );
+        }
+        if( m_quitGame.action.triggered )
+		{
+            Application.Quit();
+		}
+	}
 }
