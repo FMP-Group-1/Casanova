4a3c2657276664239c412074ced601bdb1749358
Charlie Taylor <charlie.taylor2@mail.bcu.ac.uk>
2022-06-29 14:50:23 +0100

    Made it time good and made a test scene

---
+++ Project Casanova/Assets/Louise's Stuff/Tests/LL_Enemy/TestShaders.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class TestShaders : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip( "Action Input" )]
+    private InputActionReference m_action;
+
+    private Material m_material;
+
+
+	private void OnEnable()
+	{
+        m_action.action.Enable();
+	}
+	// Start is called before the first frame update
+	void Start()
+    {
+        m_material = GetComponent<MeshRenderer>().sharedMaterial;
+        m_material.SetFloat( "_FadeStartTime", float.MaxValue );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if( m_action.action.triggered )
+		{
+            m_material.SetFloat( "_FadeStartTime", Time.time );
+            Debug.Log( "Ya boi" );
+		}
+    }
+}
