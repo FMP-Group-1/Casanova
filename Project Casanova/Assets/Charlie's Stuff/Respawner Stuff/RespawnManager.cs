41e4dce808eac58b7d62670fb5c358a4995ee3cf
Charlie Taylor <charlie.taylor2@mail.bcu.ac.uk>
2022-07-28 10:45:25 +0100

    Baked Lighting

--- Project Casanova/Assets/Charlie's Stuff/Respawner Stuff/RespawnManager.cs
+++ Project Casanova/Assets/Charlie's Stuff/Respawner Stuff/RespawnManager.cs
@@ -7,7 +7,8 @@ public enum RespawnPoint
     Cell,
     Hall,
     Armory,
-    Final
+    GuardRoom,
+    Arena
 }
 public class RespawnManager : MonoBehaviour
 {
@@ -16,12 +17,37 @@ public class RespawnManager : MonoBehaviour
     void Start()
     {
 
-
-}
+    }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void CompleteCorridor()
+    {
+        currentRespawnPoint = RespawnPoint.Hall;
+    }
+    public void CompleteArmory()
+    {
+
+        currentRespawnPoint = RespawnPoint.Armory;
+    }
+    public void CompleteGuardRoom()
+    {
+        currentRespawnPoint = RespawnPoint.GuardRoom;
+    }
+    public void SpawnInArena( int wave )
+    {
+        currentRespawnPoint = RespawnPoint.Arena;
+        switch( wave )
+		{
+            default:
+                break;
+
+            case 1:
+                break;
+		}
     }
 }
